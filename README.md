1c55eefe69ace240e92b81d917742f676446e861
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-09-06 14:41:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# FinanTracker
+
+Um app web de gerenciamento de gasto inicialmente pensado para resolver uma dor. <br/>
+Atualmente morando fora do Brasil, tive a necessidade de fazer cambio entre moedas e isso dificultava um pouco na hora de planifica-los. <br/>
+Traz o controle dos fluxo de gastos e ganhos diários, uso de duas moedas, uma principal e outra estrangeira, traz a cotação atual direto da API da Bitso ou inserir o valor manualmente. <br/>
+Futuramente será criada uma API Rest para integrar a interface.<br/>
+Em desenvolvimento... <br/>
+Aberto a qualquer contribuição.
+
+## Índice
+1. [Descrição](#descrição)
+2. [Instalação](#instalação)
+
+## Descrição
+Descreva brevemente o que o projeto faz, por que ele é útil e quem são os principais usuários.
+
+## Instalação
+
+### Pré-requisitos
+Liste os pré-requisitos para rodar o projeto, como por exemplo:
+- Node.js versão 14+
+- NPM ou Yarn
+
+### Passos para Instalação
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/Manaure-Vasconcelos/finantracker.git
+2. Instale as dependências:
+    ```bash
+    npm install
+2. Rode o projeto:
+    ```bash
+    npm run dev
---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='bg-black text-white min-h-screen w-screen px-4 py-6'>
+			{/* <div className='flex justify-between items-center mb-4'>
+				<div className='flex items-center'>
+					<div className='w-8 h-8 bg-blue-400 rounded-full flex items-center justify-center'></div>
+					<div className='ml-2'>Septiembre</div>
+				</div>
+			</div>
+
+			<div className='text-center mb-6'>
+				<p className='text-gray-400'>Saldo actual en cuentas</p>
+				<h1 className='text-3xl font-bold'>R$548.02</h1>
+				<div className='flex justify-around mt-4'>
+					<div className='flex items-center'>
+						<div className='text-green-400'>↑</div>
+						<p className='ml-2'>R$110.00</p>
+					</div>
+					<div className='flex items-center'>
+						<div className='text-red-400'>↓</div>
+						<p className='ml-2'>R$10.00</p>
+					</div>
+				</div>
+			</div>
+
+			<div className='bg-gray-800 rounded-lg p-4 mb-6'>
+				<h2 className='text-gray-400 mb-4'>Cuentas</h2>
+				<div className='flex justify-between items-center mb-4'>
+					<div className='flex items-center'>
+						<div className='w-8 h-8 bg-yellow-400 rounded-full flex items-center justify-center'>
+							💸
+						</div>
+						<div className='ml-2'>Other</div>
+					</div>
+					<p>R$448.02</p>
+				</div>
+				<div className='flex justify-between items-center'>
+					<div className='flex items-center'>
+						<div className='w-8 h-8 bg-blue-400 rounded-full flex items-center justify-center'>
+							👤
+						</div>
+						<div className='ml-2'>Pessoal</div>
+					</div>
+					<p>R$100.00</p>
+				</div>
+				<div className='text-right mt-4'>
+					<p>Total: R$548.02</p>
+				</div>
+			</div>
+
+			<div className='bg-gray-800 rounded-lg p-4'>
+				<h2 className='text-gray-400 mb-4'>Balance mensual</h2>
+				<div className='flex justify-between items-center mb-4'>
+					<p>Ingresos</p>
+					<p className='text-green-400'>R$110.00</p>
+				</div>
+				<div className='flex justify-between items-center mb-4'>
+					<p>Gastos</p>
+					<p className='text-red-400'>R$10.00</p>
+				</div>
+				<div className='flex justify-between items-center'>
+					<p>Balance</p>
+					<p>R$100.00</p>
+				</div>
+			</div>
+
+			<div className='fixed w-full bottom-0 left-0 right-0 bg-gray-900 text-white p-4 flex justify-between'>
+				<button>🏠 </button>
+				<button>📊 </button>
+				<button className='bg-purple-600 p-4 rounded-full'>＋</button>
+				<button>💼 </button>
+				<button>🔧 </button>
+			</div> */}
+		</div>
+	);
+}
+
+export default App;
