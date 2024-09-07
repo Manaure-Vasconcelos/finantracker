fbe792df5f08861214aec4cdb9edffaec3d4b9dc
Manaure Vasconcelos <manaurevasc97@gmail.com>
2024-09-07 15:14:28 -0300

    feat: add a charts component

--- src/components/BalanceChart.tsx
+++ src/components/BalanceChart.tsx
@@ -1,4 +1,69 @@
+"use client";
+
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+const chartData = [
+  { daysOfWeek: "Sunday", ingressos: 150, gastos: 60 },
+  { daysOfWeek: "Monday", ingressos: 186, gastos: 80 },
+  { daysOfWeek: "Tuesday", ingressos: 305, gastos: 200 },
+  { daysOfWeek: "Wednesday", ingressos: 237, gastos: 120 },
+  { daysOfWeek: "Thursday", ingressos: 73, gastos: 190 },
+  { daysOfWeek: "Friday", ingressos: 209, gastos: 130 },
+  { daysOfWeek: "Saturday", ingressos: 214, gastos: 140 },
+];
+
+const chartConfig = {
+  ingressos: {
+    label: "Ingressos",
+    color: "#4ade80",
+  },
+  gastos: {
+    label: "Gastos",
+    color: "#ef4444",
+  },
+} satisfies ChartConfig;
+
 function BalanceChart() {
+  return (
+    <Card className="bg-slate-800">
+      <CardHeader>
+        <CardTitle className="text-muted-foreground">Resumen Semanal</CardTitle>
+      </CardHeader>
+      <CardContent className="mt-3">
+        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="daysOfWeek"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <ChartLegend content={<ChartLegendContent />} />
+            <Bar dataKey="ingressos" fill="var(--color-ingressos)" radius={4} />
+            <Bar dataKey="gastos" fill="var(--color-gastos)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default BalanceChart;
+
+/* function BalanceChart() {
   return (
     <div className="bg-gray-800 rounded-lg p-4">
       <h2 className="text-gray-400 mb-4">Balance mensual</h2>
@@ -18,4 +83,4 @@ function BalanceChart() {
   );
 }
 
-export default BalanceChart;
+export default BalanceChart; */
--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -4,9 +4,9 @@ import BalanceOverview from "../components/BalanceOverview";
 
 function HomePage() {
   return (
-    <div className="bg-black  text-white min-h-screen w-screen">
+    <div className="bg-black  text-white min-h-screen w-screen pb-20">
       <BalanceOverview />
-      <div className="px-4 mt-5 flex flex-col gap-5">
+      <div className="px-4 mt-5 flex flex-col gap-5 scroll-p-9">
         <Accounts />
 
         <BalanceChart />
