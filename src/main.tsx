fd6a1366a861ded2bda02aab73a777567d220637
manaure <manaurevasc97@gmail.com>
2024-11-07 11:38:45 -0300

    chore: Add config initial clerk auth

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from '@clerk/clerk-react'
+import { env } from './env.ts'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ClerkProvider publishableKey={env.VITE_CLERK_PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <App />
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 );
